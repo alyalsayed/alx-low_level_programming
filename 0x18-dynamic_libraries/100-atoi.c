caeb1288a77fc6ec056be93876ffc12d585d4c97
alyalsayed <alyalsayed56@yahoo.com>
2023-05-22 07:03:58 +0300

    done: uploading C files

---
+++ 0x18-dynamic_libraries/100-atoi.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _atoi - convert string to numbers
+ *
+ * Return: n
+ */
+int _atoi(void)
+{
+	/* int x; */
+	/* char arr[]; */
+
+	/* while (*s) */
+	/* { */
+	/* x++; */
+	/* if (*s >= 48 && *s <= '9') */
+	/* conv[i] = *s */
+	/* } */
+
+	return (0);
+}
